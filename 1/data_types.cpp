0881ae6ba6c3479750084b2b6e0b16e0359259d3
cherry4xo <1jlaketuk1@gmail.com>
2023-07-04 15:56:47 +0300

    upd: 1st theme(data types)

---
+++ 1/data_types.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    // fundamental data types
+
+    int a = 69; // integer data type (2 or 4 bytes)
+    printf("int size = %d\n", sizeof(a));
+
+    float b = 329.123; // floating-point data type (4 bytes)
+    printf("float size = %d\n", sizeof(b));
+
+    double c = 3768.123083129; // double floating-point data type (8 bytes)
+    printf("double size = %d\n", sizeof(c));
+
+    char d = 57; // character data type (1 byte) 
+    printf("char size = %d\n", sizeof(d));
+
+    wchar_t e = 1234; // wide character (4 bytes)
+    printf("w_char_t size = %d\n", sizeof(e));
+
+    bool f = 1; // boolean data type (1 byte)
+    printf("bool size = %d\n", sizeof(f));
+
+    void; // void data type (0 bytes)
+
+    return 0;
+}
