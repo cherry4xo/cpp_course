6308ffabbd5816e150c8f01a3034bded5e469f37
cherry4xo <1jlaketuk1@gmail.com>
2023-07-14 14:09:00 +0300

    upd: OOP multi-inheritance.cpp

--- 6/intermediate/multi_inheritance.cpp
+++ 6/intermediate/multi_inheritance.cpp
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <vector>
 #include <string>
+#include <math.h>
 
 enum class Class_type
 {
@@ -33,18 +34,82 @@ public:
     u_int16_t get_base_damage() { return this->base_damage; }
     u_int16_t get_mana_cost() { return this->mana_cost; }
 
-    
-
+    void use_ability() 
+    {
+        printf("Ability %s have been used!\nSpent %d mana\n%d damage dealt\n", this->name.c_str(), this->mana_cost, this->base_damage);
+    }
 };
 
 class Entity
 {
+protected:
+    u_int16_t health_points;
+    u_int16_t mana_pool;
+public:
+    Entity(u_int16_t health_points, u_int16_t mana_pool):
+        health_points(health_points), mana_pool(mana_pool) {}
 
+    void increase_health_points(u_int16_t delta_health_points) { this->health_points += delta_health_points; }
+    void increase_mana_pool(u_int16_t delta_mana_pool) { this->mana_pool += delta_mana_pool; }
+    u_int16_t get_health_points() { return this->health_points; }
+    u_int16_t get_mana_pool() { return this->mana_pool; }
+    virtual void get_damage() = 0;
 };
 
 class Hero
 {
+protected:
+    std::vector<Ability*> ability_list;
+public:
+    Hero(std::vector<Ability*> ability_list)
+    {
+        this->ability_list.assign(ability_list.begin(), ability_list.end());
+    }
+    virtual void use_ability_by_index(u_int8_t index) = 0;
+};
 
+class Player: public Entity, public Hero
+{
+private:
+    const Class_type hero_class;
+    float get_damage_class_multiplyer()
+    {
+        switch (hero_class)
+        {
+        case Class_type::melee:
+            return 1.0f;
+            break;
+        case Class_type::mage:
+            return 1.2f;
+            break;
+        case Class_type::range:
+            return 1.25f;
+            break;
+        case Class_type::cast:
+            return 1.4f;
+            break;
+        default:
+            return 1.0f;
+            break;
+        }
+    }
+    bool check_required_mana_cost(u_int16_t mana_cost) { return this->mana_pool >= mana_cost; }
+public:
+    Player(u_int16_t health_points, u_int16_t mana_pool, std::vector<Ability*> ability_list, const Class_type hero_class):
+        Entity(health_points, mana_pool), Hero(ability_list), hero_class(hero_class) {}
+    void get_damage() override 
+    {
+        this->health_points -= floor(10 * get_damage_class_multiplyer());
+    }
+    void use_ability_by_index(u_int8_t index) override
+    {
+        u_int16_t ability_mana_cost = (this->ability_list)[index]->get_mana_cost();
+        if (check_required_mana_cost(ability_mana_cost))
+        {
+            increase_mana_pool(-ability_mana_cost);
+            (this->ability_list)[index]->use_ability();
+        }
+    }
 };
 
 int main(int argc, char** argv)
